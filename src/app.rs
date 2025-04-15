cd42fc8d28f3cf301a24c0ff1955e73747efdae2
Daniel Leech <daniel@dantleech.com>
2025-04-15 21:06:46 +0100

    ABandoning

--- src/app.rs
+++ src/app.rs
@@ -422,4 +422,14 @@ impl App {
             self.sender.send(event).await.unwrap()
         };
     }
+
+    pub fn take_input_plurality(&mut self) -> u8
+    {
+        let input = String::from_iter(&self.input_plurality);
+        self.input_plurality = Vec::new();
+        match input.parse::<u8>() {
+            Ok(i) => i,
+            Err(_) => u8::MAX,
+        }
+    }
 }
